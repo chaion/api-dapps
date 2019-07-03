cc7e0d4fba5faa9832b2749a52fa2f724884cfb6
Leo Ren <leo.ren@aion.network>
2019-07-03 18:03:49 +0800

    reopen scheduled tasks

--- src/main/java/com/chaion/makkiiserver/ScheduledTasks.java
+++ src/main/java/com/chaion/makkiiserver/ScheduledTasks.java
@@ -5,6 +5,7 @@ import com.chaion.makkiiserver.services.exchange.ExchangePool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -21,7 +22,7 @@ public class ScheduledTasks {
     @Autowired
     private ExchangePool pool;
 
-//    @Scheduled(fixedRate = 30 * 60 * 1000)
+    @Scheduled(fixedRate = 30 * 60 * 1000)
     public void fetchCurrencyRate() {
         logger.info("fetch currency rate");
 
